fc6eb1049b07755094c7079d38e193b16669bf46
俞周凯 <2720460362@qq.com>
2024-01-18 15:40:19 +0800

    first

---
+++ src/main/java/com/youkeda/com/kanban/KanbanApplication.java
@@ -0,0 +1,13 @@
+package com.youkeda.com.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanApplication.class, args);
+	}
+
+}
